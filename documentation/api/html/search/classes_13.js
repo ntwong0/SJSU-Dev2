a4ddc90f96beb9da44d73dbb71dcaf95e341482e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-08 01:54:44 +0000

    Deploy kammce/SJSU-Dev2 to github.com/kammce/SJSU-Dev2.git:gh-pages

---
+++ documentation/api/html/search/classes_13.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['verificationevent',['VerificationEvent',['../de/d80/structfakeit_1_1VerificationEvent.html',1,'fakeit']]],
+  ['verificationeventhandler',['VerificationEventHandler',['../d0/dda/structfakeit_1_1VerificationEventHandler.html',1,'fakeit']]],
+  ['verificationexception',['VerificationException',['../d5/d23/structfakeit_1_1VerificationException.html',1,'fakeit']]],
+  ['verifyfunctor',['VerifyFunctor',['../d6/de7/classfakeit_1_1VerifyFunctor.html',1,'fakeit']]],
+  ['verifynootherinvocationsfunctor',['VerifyNoOtherInvocationsFunctor',['../d9/d86/classfakeit_1_1VerifyNoOtherInvocationsFunctor.html',1,'fakeit']]],
+  ['verifynootherinvocationsverificationprogress',['VerifyNoOtherInvocationsVerificationProgress',['../db/d69/classfakeit_1_1VerifyNoOtherInvocationsVerificationProgress.html',1,'fakeit']]],
+  ['verifyunverifiedfunctor',['VerifyUnverifiedFunctor',['../d1/d6e/classfakeit_1_1VerifyUnverifiedFunctor.html',1,'fakeit']]],
+  ['virtualoffsetselector',['VirtualOffsetSelector',['../d2/dcb/structfakeit_1_1VirtualOffsetSelector.html',1,'fakeit']]],
+  ['virtualtable',['VirtualTable',['../dc/d13/structfakeit_1_1VirtualTable.html',1,'fakeit']]],
+  ['virtualtable_3c_20c_2c_20baseclasses_2e_2e_2e_3e',['VirtualTable&lt; C, baseclasses...&gt;',['../dc/d13/structfakeit_1_1VirtualTable.html',1,'fakeit']]],
+  ['virtualtablebase',['VirtualTableBase',['../d0/df9/structfakeit_1_1VirtualTableBase.html',1,'fakeit']]],
+  ['vtablemethodtype',['VTableMethodType',['../df/d12/structfakeit_1_1VTableMethodType.html',1,'fakeit']]],
+  ['vtutils',['VTUtils',['../d4/d5e/classfakeit_1_1VTUtils.html',1,'fakeit']]]
+];
---
+++ documentation/api/html/search/classes_f.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['recordedmethodbody',['RecordedMethodBody',['../d8/d03/classfakeit_1_1RecordedMethodBody.html',1,'fakeit']]],
+  ['refcount',['RefCount',['../d6/df7/classfakeit_1_1RefCount.html',1,'fakeit']]],
+  ['regexmatcher',['RegexMatcher',['../da/d82/structCatch_1_1Matchers_1_1StdString_1_1RegexMatcher.html',1,'Catch::Matchers::StdString']]],
+  ['registrarfortagaliases',['RegistrarForTagAliases',['../d3/d8f/structCatch_1_1RegistrarForTagAliases.html',1,'Catch']]],
+  ['repeat',['Repeat',['../d1/dc5/structfakeit_1_1Repeat.html',1,'fakeit']]],
+  ['repeatedsequence',['RepeatedSequence',['../db/d6a/classfakeit_1_1RepeatedSequence.html',1,'fakeit']]],
+  ['repeatforever',['RepeatForever',['../d8/d85/structfakeit_1_1RepeatForever.html',1,'fakeit']]],
+  ['resultdisposition',['ResultDisposition',['../d8/d62/structCatch_1_1ResultDisposition.html',1,'Catch']]],
+  ['resultwas',['ResultWas',['../d5/d8c/structCatch_1_1ResultWas.html',1,'Catch']]],
+  ['returndefaultvalue',['ReturnDefaultValue',['../d0/d75/structfakeit_1_1ReturnDefaultValue.html',1,'fakeit']]],
+  ['returndelegatevalue',['ReturnDelegateValue',['../d9/d92/structfakeit_1_1ReturnDelegateValue.html',1,'fakeit']]],
+  ['reusablestringstream',['ReusableStringStream',['../d9/d54/classCatch_1_1ReusableStringStream.html',1,'Catch']]]
+];
